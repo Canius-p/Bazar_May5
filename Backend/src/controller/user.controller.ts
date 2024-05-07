1f64e66d8a9d4634e075f07384a18a38be48cfd8
Canius-p <boiinventer@gmail.com>
2024-05-07 23:38:54 +0545

    auth controller created

---
+++ Backend/src/controller/user.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import User from '../database/models/user.model';
+
+class authController {
+  async registerUser(req: Request, res: Response): Promise<void> {
+    const { username, email, password } = req.body;
+    if (!username || !password || !email) {
+      res.status(400).json({
+        message: 'Please provide Username,email,password',
+      });
+      return;
+    }
+
+    await User.create({
+      username,
+      email,
+      password,
+    });
+    res.status(200).json({
+      message: 'User registed successfully',
+    });
+    return;
+  }
+}
+
+export default authController;
