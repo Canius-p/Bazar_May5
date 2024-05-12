097dc1cdf3d9ba3575ca36240ff25cdb38990055
Canius-p <boiinventer@gmail.com>
2024-05-12 15:49:04 +0545

    opearation related with categories

--- Backend/src/controller/category.controller.ts
+++ Backend/src/controller/category.controller.ts
@@ -1,5 +1,5 @@
 import Category from '../database/models/category.model';
-
+import { Request, Response } from 'express';
 class categoryController {
   categoryData = [
     {
@@ -22,6 +22,78 @@ class categoryController {
       console.log('Categories seeded Successfully');
     } else {
       console.log('Cateforise already seeded');
+    }
+  }
+
+  async addcategory(req: Request, res: Response): Promise<void> {
+    const { categoryName } = req.body;
+    if (!categoryName) {
+      res.status(400).json({
+        message: 'Please Provide category name ',
+      });
+      return;
+    }
+    await Category.create({
+      categoryName,
+    });
+
+    res.status(200).json({
+      message: 'Category added sucessfully',
+    });
+  }
+  async getCategories(req: Request, res: Response) {
+    const data = Category.findAll();
+    res.status(200).json({
+      message: 'Categories fetched',
+      data,
+    });
+  }
+  async deleteCategories(req: Request, res: Response) {
+    const { id } = req.params;
+    const data = await Category.findAll({
+      where: {
+        id: id,
+      },
+    });
+    if (data.length === 0) {
+      res.status(404).json({
+        message: 'Category with that id doesnt exists',
+      });
+    } else {
+      await Category.destroy({
+        where: {
+          id: id,
+        },
+      });
+      res.status(200).json({
+        message: 'Category deleted successfylly',
+      });
+    }
+  }
+  async updateCategories(req: Request, res: Response) {
+    const { id } = req.params;
+    const { categoryName } = req.body;
+    const data = await Category.findAll({
+      where: {
+        id: id,
+      },
+    });
+    if (data.length === 0) {
+      res.status(404).json({
+        message: 'Category with that id doesnt exists',
+      });
+    } else {
+      await Category.update(
+        { categoryName },
+        {
+          where: {
+            id: id,
+          },
+        }
+      );
+      res.status(200).json({
+        message: 'Category deleted successfylly',
+      });
     }
   }
 }
